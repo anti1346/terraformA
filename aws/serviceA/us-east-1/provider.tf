02fad7b9fea1c23c7037ff3c10af2217404528a5
sangcheol.byun <sangcheol.byun@gmail.com>
2021-01-29 20:45:40 +0900

    alb update

--- aws/serviceA/us-east-1/provider.tf
+++ aws/serviceA/us-east-1/provider.tf
@@ -11,5 +11,5 @@ provider "aws" {
   # Configuration options
   shared_credentials_file = "~/.aws/credentials"
   region  = "us-east-1"
-  profile = "tfa"
+  profile = "terraformA"
 }
--- aws/serviceA/us-east-1/vpcs-2Subnets.tf
+++ aws/serviceA/us-east-1/vpcs-2Subnets.tf
@@ -28,33 +28,33 @@ resource "aws_subnet" "sangchul_vpc11-sb4" {
         Service             = "network"
     }
 }
-# # Private(DB)
-# resource "aws_subnet" "sangchul_vpc11-sb13" {
-# 	vpc_id                  = aws_vpc.sangchul_vpc11.id
-# 	cidr_block              = "10.11.13.0/24"
-# 	availability_zone       = "us-east-1a"
-#     tags = {
-#         Name                = "sangchul_vpc11-sb13"
-#         Env                 = "stg"
-#         CreateUser          = "terraform@email.com"
-#         Owner               = "iac"
-#         Role                = "subnet"
-#         Service             = "network"
-#     }
-# }
-# resource "aws_subnet" "sangchul_vpc11-sb14" {
-# 	vpc_id                  = aws_vpc.sangchul_vpc11.id
-# 	cidr_block              = "10.11.14.0/24"
-# 	availability_zone       = "us-east-1c"
-#     tags = {
-#         Name                = "sangchul_vpc11-sb14"
-#         Env                 = "stg"
-#         CreateUser          = "terraform@email.com"
-#         Owner               = "iac"
-#         Role                = "subnet"
-#         Service             = "network"
-#     }
-# }
+# Private(DB)
+resource "aws_subnet" "sangchul_vpc11-sb13" {
+	vpc_id                  = aws_vpc.sangchul_vpc11.id
+	cidr_block              = "10.11.13.0/24"
+	availability_zone       = "us-east-1a"
+    tags = {
+        Name                = "sangchul_vpc11-sb13"
+        Env                 = "stg"
+        CreateUser          = "terraform@email.com"
+        Owner               = "iac"
+        Role                = "subnet"
+        Service             = "network"
+    }
+}
+resource "aws_subnet" "sangchul_vpc11-sb14" {
+	vpc_id                  = aws_vpc.sangchul_vpc11.id
+	cidr_block              = "10.11.14.0/24"
+	availability_zone       = "us-east-1c"
+    tags = {
+        Name                = "sangchul_vpc11-sb14"
+        Env                 = "stg"
+        CreateUser          = "terraform@email.com"
+        Owner               = "iac"
+        Role                = "subnet"
+        Service             = "network"
+    }
+}
 # # Public(ELB/MGMT)
 # resource "aws_subnet" "sangchul_vpc11-sb91" {
 # 	vpc_id                  = aws_vpc.sangchul_vpc11.id
