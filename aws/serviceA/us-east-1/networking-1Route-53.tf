3099f7b3d504aae615567a212084d11118e80edc
sangcheol.byun <sangcheol.byun@gmail.com>
2021-02-02 11:28:47 +0900

    .tfstate 파일 삭제

--- aws/serviceA/us-east-1/networking-1Route-53.tf
+++ aws/serviceA/us-east-1/networking-1Route-53.tf
@@ -1,8 +1,12 @@
 ####################Route 53 Zone(도메인) 등록
 resource "aws_route53_zone" "sangchulkr_zone" {
-    name = "sangchul.kr"
+    name = "sangchul.kr."
     comment = "Hosted Zone for sangchul_kr"
 }
+
+# output "ns" {
+#     value = data.aws_route53_zone.sangchulkr_zone.name_servers
+# }
 
 ####################Route 53 MX Record(서브 도메인) 등록(G. Suite)
 resource "aws_route53_record" "sangchul_kr_mx" {
@@ -27,23 +31,23 @@ resource "aws_route53_record" "sangchul_kr_mx" {
 #     ttl     = "300"
 #     records = ["111.111.111.111"]
 # }
-resource "aws_route53_record" "sangchul_kr" {
-    zone_id = aws_route53_zone.sangchulkr_zone.zone_id
-    name    = "sangchul.kr"
-    type    = "A"
-    alias {
-        name                    = aws_alb.sangchul-alb-web1.dns_name
-        zone_id                 = aws_route53_zone.sangchulkr_zone.id
-        evaluate_target_health  = true
-    }
-}
+# resource "aws_route53_record" "sangchul_kr" {
+#     zone_id = aws_route53_zone.sangchulkr_zone.zone_id
+#     name    = "sangchul.kr"
+#     type    = "A"
+#     alias {
+#         name                    = aws_alb.sangchul-alb-web1.dns_name
+#         zone_id                 = aws_route53_zone.sangchulkr_zone.id
+#         evaluate_target_health  = true
+#     }
+# }
 
-####################Route 53 CNAME Record(서브 도메인) 등록
-resource "aws_route53_record" "www_sangchul_kr_cname" {
-    zone_id = aws_route53_zone.sangchulkr_zone.zone_id
-    name    = "www.sangchul.kr"
-    type    = "CNAME"
-    ttl     = "300"
-    #records = aws_instance.sangchul_web31.public_ip
-    records = ["sangchul.kr"]
-}
+# ####################Route 53 CNAME Record(서브 도메인) 등록
+# resource "aws_route53_record" "www_sangchul_kr_cname" {
+#     zone_id = aws_route53_zone.sangchulkr_zone.zone_id
+#     name    = "www.sangchul.kr"
+#     type    = "CNAME"
+#     ttl     = "300"
+#     #records = aws_instance.sangchul_web31.public_ip
+#     records = ["sangchul.kr"]
+# }
