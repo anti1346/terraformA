b2af70c7b86ff43d54503a0f02a0de71c7a6cf2c
sangcheol.byun <sangcheol.byun@gmail.com>
2021-02-03 17:23:05 +0900

    alb 추가

--- aws/serviceA/us-east-1/identity-1Certificate_Manager.tf
+++ aws/serviceA/us-east-1/identity-1Certificate_Manager.tf
@@ -1,13 +1,13 @@
 ####################ACM SSL 인증서 생성
-resource "aws_acm_certificate" "sangchulkr" {
-    domain_name       = "sangchul.kr"
-    subject_alternative_names = [ "*.sangchul.kr" ]
-    validation_method = "DNS"
+resource "aws_acm_certificate" "tf4wxyzcom" {
+    domain_name                 = "tf.4wxyz.com"
+    subject_alternative_names   = [ "*.tf.4wxyz.com" ]
+    validation_method           = "DNS"
     lifecycle {
         create_before_destroy = true
     }
     tags = {
-        Name                = "sangchulkr"
+        Name                = "tf4wxyzcom"
         Env                 = "stg"
         CreateUser          = "terraform@email.com"
         Owner               = "iac"
@@ -17,9 +17,9 @@ resource "aws_acm_certificate" "sangchulkr" {
 }
 
 ####################Route 53 도메인 이름 검증
-resource "aws_route53_record" "sangchulkr" {
+resource "aws_route53_record" "tf4wxyzcom" {
     for_each = {
-        for dvo in aws_acm_certificate.sangchulkr.domain_validation_options : dvo.domain_name => {
+        for dvo in aws_acm_certificate.tf4wxyzcom.domain_validation_options : dvo.domain_name => {
             name   = dvo.resource_record_name
             record = dvo.resource_record_value
             type   = dvo.resource_record_type
@@ -30,11 +30,16 @@ resource "aws_route53_record" "sangchulkr" {
     records         = [each.value.record]
     ttl             = 60
     type            = each.value.type
-    zone_id         = aws_route53_zone.sangchulkr.zone_id
+    zone_id         = aws_route53_zone.tf4wxyzcom.zone_id
 }
 
 ####################ACM 인증서 유효성 검사
-resource "aws_acm_certificate_validation" "sangchulkr" {
-    certificate_arn         = aws_acm_certificate.sangchulkr.arn
-    validation_record_fqdns = [for record in aws_route53_record.sangchulkr : record.fqdn]
+resource "aws_acm_certificate_validation" "tf4wxyzcom" {
+    certificate_arn         = aws_acm_certificate.tf4wxyzcom.arn
+    validation_record_fqdns = [for record in aws_route53_record.tf4wxyzcom : record.fqdn]
+}
+
+output "acm_certificate_dns_validation_records" {
+    description = "record which is used to validate acm certificate"
+    value       = aws_acm_certificate.tf4wxyzcom.*.domain_validation_options
 }
